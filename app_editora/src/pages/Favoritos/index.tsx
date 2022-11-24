2f0eba321e61529ac38e015f0ea5d5cf55008e64
Gabriel Freitas <gabrielfn.neves@gmail.com>
2022-11-24 19:03:58 -0300

    Criação da tela de favoritos

--- app_editora/src/pages/Favoritos/index.tsx
+++ app_editora/src/pages/Favoritos/index.tsx
@@ -1,15 +1,70 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   View,
   Text,
+  FlatList,
+  StyleSheet,
+  StatusBar,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+  Image
 } from 'react-native';
+import { DadosLivroType } from '../../models/DadosLivrosType';
+import { storeLocalData, incrementLocalData, retrieveLocalData, removeLocalData } from '../../services/LocalStorageService';
+
+
+const Item = ({ item }) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{item}</Text>
+  </View>
+);
 
 const Favoritos = () => {
-  return(
-    <View>
-      <Text>Favoritos</Text>
-    </View>
-  )
+  
+  const [data, setData] = useState<DadosLivroType[]>([]);
+  
+  const handleFetchData = async () => {
+    const response = await retrieveLocalData('favoritos');
+    const temp = JSON.parse(response);
+    console.log(JSON.stringify(temp));
+    setData(response ? JSON.parse(response) : []);
+
+  }
+  
+  useEffect(() => {
+    handleFetchData();
+  }, []);
+
+  const renderItem = ({ item }) => (
+    <Item item={item.nomeLivro} />
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.codigoLivro}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
 
 export default Favoritos;
